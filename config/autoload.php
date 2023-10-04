bb1a0b992a1dba2463f6f44c7e140ce1ae1b3f68
MikaëlLESGUER <mikedev35@outlook.fr>
2023-10-04 20:15:27 +0000

    mise en place des templates

--- config/autoload.php
+++ config/autoload.php
@@ -1,15 +1,28 @@
-<?php  
-  
-    // models  
-     
-      
-    // managers  
-    
-      
-    // controllers  
-    require "controllers/AbstractController.php";  
-    require "controllers/HomeController.php";
-    require "controllers/PortfolioController.php";
-      
-    // services  
-    require "services/Router.php";
+<?php
+
+// models
+require "models/Admin.php";
+require "models/Category.php";
+require "models/Prestation.php";
+require "models/Role.php";
+require "models/User.php";
+
+// managers
+require "managers/AbstractManager.php";
+require "managers/AdminManager.php";
+require "managers/CategoryManager.php";
+require "managers/PrestationManager.php";
+require "managers/RoleManager.php";
+require "managers/UserManager.php";
+
+// controllers
+require "controllers/AbstractController.php";
+require "controllers/AdminController.php";
+require "controllers/AuthController.php";
+require "controllers/HomeController.php";
+require "controllers/PortfolioController.php";
+require "controllers/PrestationsController.php";
+require "controllers/UserController.php";
+
+// services
+require "services/Router.php";
